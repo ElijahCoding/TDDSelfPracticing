81ee4d75f94d9ec484b63bacb6337f12b040863f
Elijah <elijahsharper1@gmail.com>
2018-01-11 13:29:43 +0300

    CreateThreadTest and Refactoring

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -9,7 +9,7 @@ class ThreadController extends Controller
 {
     public function __construct()
     {
-      $this->middleware(['auth'])->only('store');
+      $this->middleware(['auth'])->only(['store','create']);
     }
 
     /**
@@ -31,7 +31,7 @@ class ThreadController extends Controller
      */
     public function create()
     {
-        //
+        return view('threads.create');
     }
 
     /**
