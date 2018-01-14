e8f7d967e10c083ff71a3acc4969c7f8f30a7748
Elijah <elijahsharper1@gmail.com>
2018-01-15 02:51:04 +0300

    Popular Threads

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -8,12 +8,19 @@ use Illuminate\Http\Request;
 
 class ThreadFilters extends Filters
 {
-  protected $filters = ['by'];
-  
+  protected $filters = ['by','popularity'];
+
   protected function by($username)
   {
     $user = User::where('name', $username)->firstOrFail();
 
     return $this->builder->where('user_id', $user->id);
+  }
+
+  protected function popularity()
+  {
+    $this->builder->getQuery()->orders = [];
+    
+    return $this->builder->orderBy('replies_count','desc');
   }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,21 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <style>
+      body {
+        padding-bottom: 100px;
+      }
+
+      .level {
+        display:flex;
+        align-items: center;
+      }
+
+      .flex {
+        flex: 1;
+      }
+    </style>
 </head>
 <body style="padding-bottom: 10px;">
     <div id="app">
@@ -46,6 +61,8 @@
                             @if (auth()->check())
                                 <li><a href="/threads?by={{ auth()->user()->name }}">My Threads</a></li>
                             @endif
+
+                            <li><a href="/threads?popularity=1">Popular Threads</a></li>
                         </ul>
                     </li>
 
