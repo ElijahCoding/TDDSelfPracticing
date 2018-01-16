28114ec5c102901b5b57ba16c2d3d6e226c97ebc
Elijah <elijahsharper1@gmail.com>
2018-01-16 18:45:13 +0300

    100% Refactorying and UserProfile, ProfilesTest

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/threads/{channel}/{thread}', 'ThreadController@show');
 Route::post('/threads/{channel}/{thread}/replies','ReplyController@store');
 
 Route::post('/replies/{reply}/favorites','FavoriteController@store');
+
+
+Route::get('/profiles/{user}','ProfileController@show');
---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class UserTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_has_many_threads()
+    {
+        $user = create('App\User');
+        $thread = create('App\Thread', ['user_id' => $user->id]);
+
+        $this->assertTrue($user->threads->contains($thread));
+    }
+}
