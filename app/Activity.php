21ba51bf68c90b6832b94b8513cebd4ac4fb1b51
Elijah <elijahsharper1@gmail.com>
2018-01-17 21:31:40 +0300

    Activities functions extracting to controllers and to Activity Model as a static function

--- app/Activity.php
+++ app/Activity.php
@@ -13,8 +13,10 @@ class Activity extends Model
       return $this->morphTo();
     }
 
-    // public static function feed()
-    // {
-    //
-    // }
+    public static function feed($user, $take = 50)
+    {
+      return $user->activity()->latest()->with('subject')->take($take)->get()->groupBy(function($activity) {
+        return $activity->created_at->format('Y-m-d');
+      });
+    }
 }
--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -10,19 +10,19 @@ class ProfileController extends Controller
 {
     public function show(User $user)
     {
-      $activities = $this->getActivity($user);
+      // $activities = $this->getActivity($user); check down getActivity() function
       // return $activities; for debugging
 
       return view('profiles.show', [
             'profileUser' => $user,
-            'activities' => $activities
+            'activities' => Activity::feed($user)
         ]);
     }
 
-    protected function getActivity(User $user)
-    {
-      return $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function($activity) {
-        return $activity->created_at->format('Y-m-d');
-      });
-    }
+    // protected function getActivity(User $user)
+    // {
+    //   return $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function($activity) {
+    //     return $activity->created_at->format('Y-m-d');
+    //   });
+    // }
 }
